17390afad375938ee659e2530d518a872423ee2b
Asmi jain <asmidev29@gmail.com>
2023-03-25 11:27:24 +0530

    Initial Commit

---
+++ CombineNetworking/CombineNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNetworkingApp.swift
+//  CombineNetworking
+//
+//  Created by ASMI iOS Dev on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
